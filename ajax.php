5c0667528a45a1d1ee69029d624382d42e56537d
marco <marco@192.168.15.4>
2021-03-26 22:28:58 -0300

    Feita verificação de nome unico

--- ajax.php
+++ ajax.php
@@ -17,6 +17,32 @@ function pr($dado, $print_r = true) {
     }
 }
 
+function listar($id = '', $NOME = '') {
+    global $PDO;
+
+    $WHERE = [];
+    $EXECUTE = [];
+    if ($id) {
+        $WHERE[] = ' ID_PESSOA = :ID_PESSOA ';
+        $EXECUTE[':ID_PESSOA'] = $id;
+    }
+    if ($NOME) {
+        $WHERE[] = ' NOME = :NOME ';
+        $EXECUTE[':NOME'] = $NOME;
+    }
+    $where = '';
+    if ($WHERE) {
+        $where = ' WHERE ' . implode(' AND ', $WHERE);
+    }
+
+    $sql = "SELECT * FROM PESSOA $where ORDER BY NOME";
+    $prepare = $PDO->prepare($sql);
+    $prepare->execute($EXECUTE);
+
+    $DADOS = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    return $DADOS;
+}
+
 //RETORNO - inicio
 $retorno = [
     'status' => 'erro',
@@ -39,24 +65,27 @@ try {
 
     //LISTAR
     if (@$_POST['ACAO'] == 'Listar') {
-        $query = $PDO->query('
-            SELECT * FROM PESSOA ORDER BY NOME
-        ');
-        $DADOS = $query->fetchAll(PDO::FETCH_ASSOC);
+        $DADOS = listar();
         $retorno['status'] = 'ok';
         $retorno['mensagem'] = 'Pessoas listadas';
-        $retorno['lista'] = $DADOS;
+        $retorno['lista'] = @$DADOS;
     }
     //INCLUIR
     elseif (@$_POST['ACAO'] == 'Incluir') {
         $prepare = $PDO->prepare('
             INSERT INTO PESSOA (NOME) VALUES (:NOME)
         ');
-        $prepare->execute([
-            ':NOME' => @$_POST['NOME']
-        ]);
-        $retorno['status'] = 'ok';
-        $retorno['mensagem'] = "$_POST[NOME] incluído(a)";
+        $DADO = @listar('', @$_POST['NOME'])[0];
+        if ($DADO) {
+            $retorno['status'] = 'erro';
+            $retorno['mensagem'] = "$_POST[NOME] já existe";
+        } else {
+            $prepare->execute([
+                ':NOME' => @$_POST['NOME']
+            ]);
+            $retorno['status'] = 'ok';
+            $retorno['mensagem'] = "$_POST[NOME] incluído(a)";
+        }
     }
     //EXCLUIR
     elseif (@$_POST['ACAO'] == 'Excluir') {
@@ -71,16 +100,10 @@ try {
     }
     //Consulta
     elseif (@$_POST['ACAO'] == 'Buscar') {
-        $prepare = $PDO->prepare('
-            SELECT * FROM PESSOA WHERE ID_PESSOA = :ID_PESSOA
-        ');
-        $prepare->execute([
-            ':ID_PESSOA' => @$_POST['ID_PESSOA']
-        ]);
-        $DADO = $prepare->fetch(PDO::FETCH_ASSOC);
+        $DADO = listar(@$_POST['ID_PESSOA']);
         $retorno['status'] = 'ok';
         $retorno['mensagem'] = 'Pessoa listada';
-        $retorno['dado'] = $DADO;
+        $retorno['dado'] = $DADO[0];
         if (!$DADO) {
             $retorno['status'] = 'erro';
             $retorno['mensagem'] = 'Pessoa não localizada';
